7aa3608a876a5e58f141645c48f76f8153fc1a57
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-26 23:29:21 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,9 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
+  brands = ["Uggs", "Rainbow", "Uggs", "Rainbow", "Nike", "Nike"]
 
   def initialize(brand)
     @brand = brand
@@ -9,6 +12,16 @@ class Shoe
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+  end
+  
+  brands.each_with_index do |brand, index|
+    shoe = Shoe.new("Shoe_#{i}")
+    shoe.brand = brand
+  end
+  
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand.clear
   end
 
 end
